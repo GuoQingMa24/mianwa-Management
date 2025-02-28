9ce84c0c78367124c5a130f2be6e3fcb25b92eb4
cqupt-pengyuyan <116574444+GuoQingMa24@users.noreply.github.com>
2025-02-28 15:55:09 +0800

    feat-添加表格

--- mianwa/src/components/Charts.vue
+++ mianwa/src/components/Charts.vue
@@ -1,11 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div id="chart" style="width: 600px; height: 400px;"></div>
 </template>
 
-<style scoped>
+<script>
+import * as echarts from 'echarts';
 
-</style>
+export default {
+  mounted() {
+    // 基于准备好的 DOM，初始化 echarts 实例
+    const myChart = echarts.init(this.$el.querySelector('#chart'));
+
+    // 指定图表的配置项和数据
+    const option = {
+      title: {
+        text: 'ECharts 示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表
+    myChart.setOption(option);
+  }
+};
+</script>
